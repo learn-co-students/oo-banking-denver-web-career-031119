71d8099c96902f653458b1388685cd27a37af300
ibenthinkin <benjamin.reilly@gmail.com>
2019-03-14 15:11:10 -0600

    built execute_transaction and reverse_transaction

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,9 +19,21 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       self.status = "complete"
-    else
+    end
+
+    if sender.valid? == false
+      self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += @amount
+      receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
+
 
 end
